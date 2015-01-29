921f73b7aba108a27568a4cafad26ffd22e4b412
nickmcblain <nickmcblain@gmail.com>
2015-01-29 23:27:08 +0000

    Image changed from array to single string

--- server.js
+++ server.js
@@ -91,8 +91,6 @@ router.route('/tasks')
 	.post(auth.isAuthenticated, function(req, res){
 		var task = new Task();
 
-		console.log(req.body.images.url);
-
 		task.title = req.body.title;
 		task.tags = req.body.tags;
 		task.images = req.body.images;
