293e1536f7013f63790db0d506522a45d95536db
Nicholas McBlain <nickmcblain@gmail.com>
2015-01-31 23:10:33 +0000

    Check to see if API recieves data

--- server.js
+++ server.js
@@ -91,6 +91,8 @@ router.route('/tasks')
 	.post(auth.isAuthenticated, function(req, res){
 		var task = new Task();
 
+		console.log(req.body.image);
+
 		task.title = req.body.title;
 		task.tags = req.body.tags;
 		task.image = req.body.image;
